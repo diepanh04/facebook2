5b81a6959fd70ee1ae8b5ee936925041ea9fdc85
diepanh04 <ngdiepanh2002@gmail.com>
2022-07-04 14:01:38 +0700

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
       user.avatar = auth.info.avatar # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
-      user.skip_confirmation!
+      # user.skip_confirmation!
     end
   end
 
